5bf6a8bf87fb9404c790d6088e11d84ace1cfaee
fran714 <67741672+fran714@users.noreply.github.com>
2021-12-02 11:00:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,10 @@ import os
 import glob
 cookie_del = glob.glob("config/*cookie.json")
 os.remove(cookie_del[0])
-
 from instabot import Bot
-
 bot = Bot()
-
-bot.login(username="sudoalpha26",password="promisecare")
-
-
-bot.upload_photo("fire.jpg", caption="Fireworks Happy New Month")
+bot.login(username="Enter Your UserName Here ",password=" Enter Your Account Password Here")
+bot.upload_photo("galo.jpg", caption="Enter Your caption here")
 
 
 
